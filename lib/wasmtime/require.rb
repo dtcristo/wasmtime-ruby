1c1afb2e4d5376b7e6751b49ea93af3839023772
David Cristofaro <david@dtcristo.com>
2019-09-01 22:49:09 +1000

    Cleanup require patch

--- lib/wasmtime/require.rb
+++ lib/wasmtime/require.rb
@@ -1,5 +1,5 @@
 module Kernel
-  unless defined?(wasmtime_original_require)
+  unless defined? wasmtime_original_require
     alias_method :wasmtime_original_require, :require
     private :wasmtime_original_require
   end
@@ -10,13 +10,11 @@ module Kernel
     $LOAD_PATH.each do |load_dir|
       path = "#{path}.wasm" unless path.end_with?('.wasm')
       wasm_path = File.expand_path(path, load_dir)
+      return false if $LOADED_FEATURES.include?(wasm_path)
       if File.file?(wasm_path)
-        if $LOADED_FEATURES.include?(wasm_path)
-          return false
-        else
-          $LOADED_FEATURES << wasm_path
-          return true
-        end
+        # TODO: Load WASM module here
+        $LOADED_FEATURES << wasm_path
+        return true
       end
     end
     raise load_error
