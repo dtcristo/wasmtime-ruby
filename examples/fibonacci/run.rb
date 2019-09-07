33fae402d5321349ad282def7042ef0fc47b032f
David Cristofaro <david@dtcristo.com>
2019-09-07 16:47:28 +1000

    Add fibonacci test

--- examples/fibonacci/run.rb
+++ examples/fibonacci/run.rb
@@ -1,4 +1,4 @@
 require 'wasmtime'
 require_relative 'target/wasm32-unknown-unknown/release/fibonacci'
 
-puts Fibonacci.fib(5)
+puts Fibonacci.fib(11)
--- test/wasmtime/instance_test.rb
+++ test/wasmtime/instance_test.rb
@@ -4,10 +4,18 @@ require 'test_helper'
 
 module Wasmtime
   class InstanceTest < Minitest::Test
-    def test_invoke_with_markdown_render
+    def test_invoke_with_markdown
       instance = Instance.new('examples/markdown/markdown.wasm')
       result = instance.invoke('render', ['# Hello, Ruby!'])
       assert_equal "<h1>Hello, Ruby!</h1>\n", result
+    end
+
+    def test_invoke_with_fibonacci
+      instance =
+        Instance.new(
+          'examples/fibonacci/target/wasm32-unknown-unknown/release/fibonacci.wasm'
+        )
+      assert_equal 89, instance.invoke('fib', [11])
     end
 
     def test_exports_includes_function
