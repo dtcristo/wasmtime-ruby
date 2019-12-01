37ddb0ce221bd6dc916c0c792d00f9e0b88f8863
David Cristofaro <david@dtcristo.com>
2019-12-01 21:25:59 +1100

    Update WasmValue

--- src/wasm_value.rs
+++ src/wasm_value.rs
@@ -1,19 +1,19 @@
 use rutie::rubysys::value::ValueType;
 use rutie::{AnyObject, Fixnum, Float, Object, RString};
 use std::convert::TryInto;
-use wasmtime_interface_types::Value;
+use wasmtime_interface_types as wit;
 
 #[derive(Clone, Debug)]
-pub struct WasmValue(pub Value);
+pub struct WasmValue(pub wit::Value);
 
-impl From<Value> for WasmValue {
-    fn from(value: Value) -> Self {
+impl From<wit::Value> for WasmValue {
+    fn from(value: wit::Value) -> Self {
         WasmValue(value)
     }
 }
 
-impl Into<Value> for WasmValue {
-    fn into(self) -> Value {
+impl Into<wit::Value> for WasmValue {
+    fn into(self) -> wit::Value {
         self.0
     }
 }
@@ -22,13 +22,13 @@ impl From<AnyObject> for WasmValue {
     fn from(object: AnyObject) -> Self {
         match object.ty() {
             ValueType::RString => {
-                Value::String(object.try_convert_to::<RString>().unwrap().to_string()).into()
+                wit::Value::String(object.try_convert_to::<RString>().unwrap().to_string()).into()
             }
             ValueType::Fixnum => {
-                Value::I32(object.try_convert_to::<Fixnum>().unwrap().to_i32()).into()
+                wit::Value::I32(object.try_convert_to::<Fixnum>().unwrap().to_i32()).into()
             }
             ValueType::Float => {
-                Value::F64(object.try_convert_to::<Float>().unwrap().to_f64()).into()
+                wit::Value::F64(object.try_convert_to::<Float>().unwrap().to_f64()).into()
             }
             _ => panic!("Unable to convert object to WasmValue"),
         }
@@ -39,13 +39,13 @@ impl Into<AnyObject> for WasmValue {
     fn into(self) -> AnyObject {
         let value = self.0;
         match value {
-            Value::String(v) => RString::new_utf8(&v).into(),
-            Value::I32(v) => Fixnum::new(v.into()).into(),
-            Value::U32(v) => Fixnum::new(v.try_into().unwrap()).into(),
-            Value::I64(v) => Fixnum::new(v).into(),
-            Value::U64(v) => Fixnum::new(v.try_into().unwrap()).into(),
-            Value::F32(v) => Float::new(v.into()).into(),
-            Value::F64(v) => Float::new(v).into(),
+            wit::Value::String(v) => RString::new_utf8(&v).into(),
+            wit::Value::I32(v) => Fixnum::new(v.into()).into(),
+            wit::Value::U32(v) => Fixnum::new(v.try_into().unwrap()).into(),
+            wit::Value::I64(v) => Fixnum::new(v).into(),
+            wit::Value::U64(v) => Fixnum::new(v.try_into().unwrap()).into(),
+            wit::Value::F32(v) => Float::new(v.into()).into(),
+            wit::Value::F64(v) => Float::new(v).into(),
         }
     }
 }
