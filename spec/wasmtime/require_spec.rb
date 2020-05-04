6272a41acacf481e0cfe0c3040f393d66f911b42
David Cristofaro <david@dtcristo.com>
2020-05-04 22:27:14 +1000

    Use fibbonacci module for require spec

--- spec/wasmtime/require_spec.rb
+++ spec/wasmtime/require_spec.rb
@@ -1,18 +1,16 @@
 # frozen_string_literal: true
 
-require 'wasmtime'
+require 'wasmtime/require'
 
-describe Wasmtime do
-  xdescribe 'require' do
-    it 'should load markdown module' do
-      expect(require_relative('../../wasm/markdown')).to be_truthy
-      markdown_lib = File.expand_path('../../wasm', __dir__)
-      $LOAD_PATH.unshift(markdown_lib) unless $LOAD_PATH.include?(markdown_lib)
-      expect(require('markdown')).to be_falsey
-      expect(require('markdown.wasm')).to be_falsey
-      expect { require('missing.wasm') }.to raise_error(LoadError)
-      result = Markdown.render('# Hello, Ruby!')
-      expect(result).to eq("<h1>Hello, Ruby!</h1>\n")
-    end
+describe 'require' do
+  it 'should load fibonacci module' do
+    expect(require_relative('../../wasm/fibonacci')).to be_truthy
+    fibonacci_lib = File.expand_path('../../wasm', __dir__)
+    $LOAD_PATH.unshift(fibonacci_lib) unless $LOAD_PATH.include?(fibonacci_lib)
+    expect(require('fibonacci')).to be_falsey
+    expect(require('fibonacci.wasm')).to be_falsey
+    expect { require('missing.wasm') }.to raise_error(LoadError)
+    result = Fibonacci.fib(11)
+    expect(result).to eq(89)
   end
 end
